4acc050b1b5b7578b37e1adb6f7e1401986d046c
shrey150 <shrey150@yahoo.com>
2019-12-07 06:51:59 -0500

    added shuffling

--- index.js
+++ index.js
@@ -178,6 +178,16 @@ function nextCard() {
 
 }
 
+function shuffle() {
+
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+
+    loadCard();
+
+}
 
 function selectAll(id) {
 
